3a6d9bc4c98eff1114d126dd26223e3d2a0a8b7f
sake <2050890266@qq.com>
2023-03-03 14:22:35 +0800

    first commit

---
+++ test.cpp
@@ -0,0 +1,21 @@
+   #include<iostream>
+   #include<string>
+   using namespace std; 
+    bool isValid(string s) {
+        bool flag=false;
+        for(int i=0;i<s.size();i++){
+            bool f=false;
+            for(int j=i+1;j<s.size();++j){
+                if(s[i]==s[j]) f=true;
+            }
+            if(f) flag=true;
+        }
+        return flag;
+    }
+    int main(){
+        string s="()";
+        s.find("s");
+        s.replace(1," ","");
+        isValid(s);
+        return 0 ;
+    }
