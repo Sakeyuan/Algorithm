be820a499901229367e2ae5e1a00301cc59478fb
sake <2050890266@qq.com>
2023-03-05 12:00:22 +0800

    算法

--- Manacher/Manacher.cpp
+++ Manacher/Manacher.cpp
@@ -1,10 +1,61 @@
 /**
  * @file Manacher.cpp
  * @author your name (you@domain.com)
- * @brief 一般用于求最长回文子串
+ * @brief 一般用于求最长回文子串(i和i'关于C对称,R为回文串半径)
+ *  1）i在R外
+ *  2) i在R内
+ *      a. i' 回文半径在R内
+ *      b. i' 回文半径左侧在边界上
+ *      c. i' 回文半径左侧在R外
  * @version 0.1
  * @date 2023-03-03
  * 
  * @copyright Copyright (c) 2023
  * 
  */
+
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<cstdlib>
+using namespace std;
+void handleStr(string& str){
+    string tmp="";
+    for(int i=0; i<str.size(); i++){
+        tmp+="#";
+        tmp+=str[i];
+    }
+    tmp += "#";
+    str=tmp;
+}
+int Manacher(string str){
+    int Max = -1;
+    int C=-1;       //直径中心  
+    int R=-1;       //半径右边界
+    handleStr(str);
+    int n=str.size();
+    int RArray[n];  //回文半径数组
+    for(int i=0; i < n; i++){
+        RArray[i]=R > i ? min(RArray[2*C-i],R-i):1;
+        while (i + RArray[i] < n &&  i - RArray[i] > -1)   //统一往外扩
+        {
+            if(str[RArray[i]+i] == str[i-RArray[i]]){
+                RArray[i]++;
+            }else{  //无法扩展
+                break;
+            }
+        } 
+        if(i+RArray[i] > R){
+            R=i+RArray[i];
+            C=i;
+        }
+        Max = max(Max,RArray[i]);
+    }
+    return Max-1;  //加特殊符号后需要-1才可以得到原来字符串的回文串长度
+}
+int main(){
+    string str="122132";
+    cout<<Manacher(str);
+    system("pause");
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,20 +1,26 @@
-   #include<iostream>
-   #include<string>
-   using namespace std; 
-    bool isValid(string s) {
-        bool flag=false;
-        for(int i=0;i<s.size();i++){
-            bool f=false;
-            for(int j=i+1;j<s.size();++j){
-                if(s[i]==s[j]) f=true;
-            }
-            if(f) flag=true;
-        }
-        return flag;
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+int trap(int* height,int n) {
+    int res = 0;
+    int leftMax[n];
+    int rightMax[n];
+    leftMax[0] = height[0];
+    rightMax[n - 1] = height[n - 1];
+    for (int i = 1; i < n - 1; ++i) {
+        leftMax[i] = max(leftMax[i - 1], height[i]);
+        rightMax[n - 1 - i] = max(rightMax[n - i], height[n - i]);
     }
-    int main(){
-        string s="()";
-        s.find("s");
-        isValid(s);
-        return 0 ;
+    for (int i = 0; i < n; ++i) {
+        res += min(leftMax[i], rightMax[i]) - height[i];
     }
+    return res;
+}
+
+int main() {
+    int num[] = { 0,1,0,2,1,0,1,3,2,1,2,1 };
+    trap(num,sizeof(num)/sizeof(num[0]));
+    return 0;
+}
